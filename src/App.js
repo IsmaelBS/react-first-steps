9fd44f3ec34d5dcf55649e3201ae7e6eb91f080b
Ismael Barboza dos Santos <ismabarboza@gmail.com>
2019-08-20 14:12:23 -0300

    Adição de estado no TechList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from "react";
-import { Fragment } from "react";
-import user from "./assets/img/User2.jpg";
 import TechList from "./components/TechList";
 
-const App = () => (
-  <Fragment>
-    <TechList />
-  </Fragment>
-);
+const App = () => <TechList />;
 
 export default App;
--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,16 +2,37 @@ import React, { Component } from "react";
 
 class TechList extends Component {
   state = {
-    techs: ["Node.js", "React", "React Native"]
+    techs: ["Node.js", "React", "React Native"],
+    newTech: ""
+  };
+
+  handleNewTechChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleNewTechSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
   };
 
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleNewTechSubmit}>
+        <ul>
+          {this.state.techs.map(el => (
+            <li key={el}>{el}</li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleNewTechChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
     );
   }
 }
